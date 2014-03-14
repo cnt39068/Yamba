5d5b628cb64d0689ec1f0ad8b169f58ae544802e
Will Chau <willc@virtual-centos.(none)>
2014-03-15 00:29:29 +0800

    add onclick

--- src/com/absolado/yamba/StatusActivity.java
+++ src/com/absolado/yamba/StatusActivity.java
@@ -2,16 +2,35 @@ package com.absolado.yamba;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener{
 
+	private static final String TAG = "StatusActivity";
+	private EditText editStatus;
+	private Button buttonTweet;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+		
+		editStatus = (EditText) findViewById(R.id.editStatus);
+		buttonTweet = (Button) findViewById(R.id.buttonTweet);
+		
+		buttonTweet.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View view) {
+		Log.d(TAG, "onClicked with status: " + editStatus.getText().toString());
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
